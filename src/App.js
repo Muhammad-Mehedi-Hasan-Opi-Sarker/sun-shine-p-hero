58bcef9004b4e7a6a3a01de51820c1a152927ce0
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-20 13:33:55 +0600

    add and info section and with style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './Pages/sheard/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Header></Header>
      <Routes>
       <Route path='/' element={<Home></Home>}></Route>
---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import about from '../../aserts/about1.png'
+import {VscCheck} from 'react-icons/vsc';
+
+const Info = () => {
+    return (
+        <div className=''>
+            <div className="lg:px-12 px-2 card lg:card-side bg-base-100 shadow-xl"
+                style={{ backgroundColor: 'rgb(251,247,245)' }}>
+                <figure className='lg:p-12 p-8'><img src={about} alt="Movie" /></figure>
+                <div className="card-body">
+                    {/* title  */}
+                    <span className='lg:mt-12'><button
+                        className='font-bold p-2'
+                        style={{ backgroundColor: 'rgb(255,228,170)' }}>About us</button></span>
+                    {/* h1  */}
+                    <h1
+                        className='font-bold text-5xl'
+                    >I help individuals to </h1>
+                    <h1 className='font-bold text-5xl mb-4'>become their best version.</h1>
+                    {/* paragraph */}
+                    <div>
+                        <p className='mb-8'>I am glad that you have made it here to send a distress signal, and
+                            inform the Senate that all on board were killed. Dantooine.
+                            I’m not going to Alderaan. I really got to go. But that to me.</p>
+                        <div className='grid lg:grid-cols-3'>
+
+                            <div className='mb-3 flex'>
+                                <span className='text-4xl mr-5 grid justify-items-center content-center' 
+                                style={{ backgroundColor: 'rgb(255,228,170)', height:'50px',width:'50px'}}
+                                ><VscCheck></VscCheck></span>
+                                <h5 className='font-bold text-lg'>
+                                    <span>Time to time</span><br />
+                                    <span>shedule</span>
+                                </h5>
+                            </div>
+
+                            <div className='mb-3 flex'>
+                            <span 
+                            style={{backgroundColor:'rgb(49,100,94)', height:'50px',width:'50px'}}
+                            className='text-4xl text-white mr-5 grid justify-items-center content-center'><VscCheck></VscCheck></span>
+                                <h5 className='font-bold text-lg'>
+                                    <span>24/7 Online</span><br />
+                                    <span>Suppport</span>
+                                </h5>
+                            </div>
+
+                            <div className='flex'>
+                            <span 
+                            style={{backgroundColor:'#FFFFFF', height:'50px',width:'50px'}}
+                            className='text-4xl mr-5 grid justify-items-center content-center'><VscCheck></VscCheck></span>
+                                <h5 className='font-bold text-lg'>
+                                    <span>Man to</span><br />
+                                    <span>Optimize</span>
+                                </h5>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
