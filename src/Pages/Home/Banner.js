f86604a5ab93ed1b62c521350cf93fe5f435bc0c
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-20 14:13:04 +0600

    complite info section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,33 +4,33 @@ import banner3 from '../../aserts/banner3.jpg';
 const banner = () => {
     return (
         <div>
-            <div class="carousel w-full h-full">
-                <div id="slide1" class="carousel-item relative w-full">
-                    <img src={banner3} class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" class="btn btn-circle">❮</a>
-                        <a href="#slide2" class="btn btn-circle">❯</a>
+            <div className="carousel w-full h-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={banner3} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide2" class="carousel-item relative w-full">
-                    <img src={banner3} class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" class="btn btn-circle">❮</a>
-                        <a href="#slide3" class="btn btn-circle">❯</a>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={banner3} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide3" class="carousel-item relative w-full">
-                    <img src={banner3} class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" class="btn btn-circle">❮</a>
-                        <a href="#slide4" class="btn btn-circle">❯</a>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={banner3} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide4" class="carousel-item relative w-full">
-                    <img src={banner3} class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" class="btn btn-circle">❮</a>
-                        <a href="#slide1" class="btn btn-circle">❯</a>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src={banner3} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import about from '../../aserts/about1.png'
-import {VscCheck} from 'react-icons/vsc';
+import { VscCheck } from 'react-icons/vsc';
 
 const Info = () => {
     return (
@@ -9,55 +9,80 @@ const Info = () => {
                 style={{ backgroundColor: 'rgb(251,247,245)' }}>
                 <figure className='lg:p-12 p-8'><img src={about} alt="Movie" /></figure>
                 <div className="card-body">
-                    {/* title  */}
-                    <span className='lg:mt-12'><button
-                        className='font-bold p-2'
-                        style={{ backgroundColor: 'rgb(255,228,170)' }}>About us</button></span>
-                    {/* h1  */}
-                    <h1
-                        className='font-bold text-5xl'
-                    >I help individuals to </h1>
-                    <h1 className='font-bold text-5xl mb-4'>become their best version.</h1>
-                    {/* paragraph */}
                     <div>
-                        <p className='mb-8'>I am glad that you have made it here to send a distress signal, and
-                            inform the Senate that all on board were killed. Dantooine.
-                            I’m not going to Alderaan. I really got to go. But that to me.</p>
-                        <div className='grid lg:grid-cols-3'>
+                        {/* title  */}
+                        <span className='lg:mt-12'><button
+                            className='font-bold p-2'
+                            style={{ backgroundColor: 'rgb(255,228,170)' }}>About us</button></span>
+                        {/* h1  */}
+                        <h1
+                            className='font-bold text-5xl'
+                        >I help individuals to </h1>
+                        <h1 className='font-bold text-5xl mb-4'>become their best version.</h1>
+                        {/* paragraph */}
+                        <div>
+                            <p className='mb-8'>I am glad that you have made it here to send a distress signal, and
+                                inform the Senate that all on board were killed. Dantooine.
+                                I’m not going to Alderaan. I really got to go. But that to me.</p>
+                            <div className='grid lg:grid-cols-3'>
 
-                            <div className='mb-3 flex'>
-                                <span className='text-4xl mr-5 grid justify-items-center content-center' 
-                                style={{ backgroundColor: 'rgb(255,228,170)', height:'50px',width:'50px'}}
-                                ><VscCheck></VscCheck></span>
-                                <h5 className='font-bold text-lg'>
-                                    <span>Time to time</span><br />
-                                    <span>shedule</span>
-                                </h5>
+                                <div className='mb-3 flex'>
+                                    <span className='text-4xl mr-5 grid justify-items-center content-center'
+                                        style={{ backgroundColor: 'rgb(255,228,170)', height: '50px', width: '50px' }}
+                                    ><VscCheck></VscCheck></span>
+                                    <h5 className='font-bold text-lg'>
+                                        <span>Time to time</span><br />
+                                        <span>shedule</span>
+                                    </h5>
+                                </div>
+
+                                <div className='mb-3 flex'>
+                                    <span
+                                        style={{ backgroundColor: 'rgb(49,100,94)', height: '50px', width: '50px' }}
+                                        className='text-4xl text-white mr-5 grid justify-items-center content-center'><VscCheck></VscCheck></span>
+                                    <h5 className='font-bold text-lg'>
+                                        <span>24/7 Online</span><br />
+                                        <span>Suppport</span>
+                                    </h5>
+                                </div>
+
+                                <div className='flex'>
+                                    <span
+                                        style={{ backgroundColor: '#FFFFFF', height: '50px', width: '50px' }}
+                                        className='text-4xl mr-5 grid justify-items-center content-center'><VscCheck></VscCheck></span>
+                                    <h5 className='font-bold text-lg'>
+                                        <span>Man to</span><br />
+                                        <span>Optimize</span>
+                                    </h5>
+                                </div>
+
                             </div>
+                        </div>
 
-                            <div className='mb-3 flex'>
-                            <span 
-                            style={{backgroundColor:'rgb(49,100,94)', height:'50px',width:'50px'}}
-                            className='text-4xl text-white mr-5 grid justify-items-center content-center'><VscCheck></VscCheck></span>
-                                <h5 className='font-bold text-lg'>
-                                    <span>24/7 Online</span><br />
-                                    <span>Suppport</span>
-                                </h5>
+                        {/* slat here  */}
+                        <div className='mt-8'>
+                            <div className="stats stats-vertical lg:stats-horizontal">
+
+                                <div className="stat flex">
+                                    <div className="stat-value">31K</div>
+                                    <div className="stat-title">Active <br /> students <br />
+                                        worldwide</div>
+                                </div>
+
+                                <div className="stat">
+                                    <div className="stat-value">10k</div>
+                                    <div className="stat-title">Basement <br />
+                                        in New York</div>
+                                </div>
+
+                                <div className="stat">
+                                    <div className="stat-value">2k</div>
+                                    <div className="stat-title">Cup of tea & <br />
+                                        coffee</div>
+                                </div>
                             </div>
-
-                            <div className='flex'>
-                            <span 
-                            style={{backgroundColor:'#FFFFFF', height:'50px',width:'50px'}}
-                            className='text-4xl mr-5 grid justify-items-center content-center'><VscCheck></VscCheck></span>
-                                <h5 className='font-bold text-lg'>
-                                    <span>Man to</span><br />
-                                    <span>Optimize</span>
-                                </h5>
-                            </div>
-
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
