6ef01a4cc08e95198b1cdc03633faf534d4f9bd1
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-21 01:40:27 +0600

    login form add

--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -5,7 +5,7 @@ const Service = ({ data }) => {
     return (
         <div>
             <div class="card w-96 bg-base-100 shadow-xl mb-5 rounded-none">
-                <figure><img src={data.img} alt="Shoes" /></figure>
+                <figure><img className='w-full' src={data.img} alt="Shoes" /></figure>
                 <div class="card-body">
                     <h3 className='font-bold text-xl'>{data.name}</h3>
                     <div class="card-actions justify-end">
--- src/Pages/sheard/Header.js
+++ src/Pages/sheard/Header.js
@@ -10,26 +10,26 @@ const Header = () => {
     </>
     return (
         <div>
-            <div class="navbar bg-base-100">
-                <div class="navbar-start">
-                    <div class="dropdown">
-                        <label tabindex="0" class="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabindex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {nav}
                         </ul>
                     </div>
-                    <a class="btn btn-ghost normal-case text-xl"><h1
-                        className='font-bold text-2xl'>EDUCATION</h1></a>
+                    <Link to='/home' className="btn btn-ghost normal-case text-xl"><h1
+                        className='font-bold text-2xl'>EDUCATION</h1></Link>
                 </div>
-                <div class="navbar-center hidden lg:flex">
-                    <ul class="menu menu-horizontal p-0">
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
                         {nav}
                     </ul>
                 </div>
-                <div class="navbar-end">
-                    <ul class="menu menu-horizontal p-0">
+                <div className="navbar-end">
+                    <ul className="menu menu-horizontal p-0">
                         <li><Link to='/login'>login</Link></li>
                     </ul>
                 </div>
